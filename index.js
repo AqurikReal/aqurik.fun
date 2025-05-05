b7455b733d441effb80c218b3e9c28909cea1bce
Aqurik <aqurik.men@gmail.com>
2025-05-05 16:05:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if (window.location.href == "http://aqurik.fun"){
-  window.location.href = "https://aqurik.fun";
+  window.location.href = "https://aqurik.fun/balls";
 }
 if (window.location.href == "http://www.aqurik.fun"){
-  window.location.href = "https://aqurik.fun";
+  window.location.href = "https://aqurik.fun/balls";
 }
