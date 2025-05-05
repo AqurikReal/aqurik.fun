11e22e72cbe234b22f6da17f8a728d0571fb7574
Aqurik <aqurik.men@gmail.com>
2025-05-05 16:07:29 +0300

    Update index.js
    fixed errors

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-if (window.location.href == "http://aqurik.fun"){
-  window.location.href = "https://aqurik.fun/balls";
-}
-if (window.location.href == "http://www.aqurik.fun"){
-  window.location.href = "https://aqurik.fun/balls";
+
+if (location.protocol !== 'https:'){
+  location.replace('https://' + location.href.split('//')[1]);
 }
