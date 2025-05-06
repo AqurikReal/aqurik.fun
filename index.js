2d181cfc775d1fbc24a5c902ab3565dcb3c8b23d
aqurik <aqurik.men@gmail.com>
2025-05-06 17:09:15 +0300

    added mobile redirect thing

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ if (location.protocol !== 'https:'){
   console.log("Redirecting to https...");
   location.replace('https://' + location.href.split('//')[1]);
 };
+
+if (navigator.userAgentData.mobile){
+  console.log("Redirecting to mobile version...")
+  location.replace('https://aqurik.fun/mobile');
+}
