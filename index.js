0cb3473326859f6725382f10c159d7933c92372b
aqurik <aqurik.men@gmail.com>
2025-05-06 18:21:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ if (location.protocol !== 'https:'){
 
 
 
-if (/Mobile/i.test(navigator.userAgent)){
+if (
+  
+ /Mobile/i.test(navigator.userAgent) ||
+ /Android/i.test(navigator.userAgent) || 
+ /Tablet/i.test(navigator.userAgent) ||
+ /IEMobile/i.test(navigator.userAgent) ||
+ /IEMobile/i.test(navigator.userAgent) ||
+ /iPad/i.test(navigator.userAgent) ||
+ /iPod/i.test(navigator.userAgent) ||
+ /iPhone/i.test(navigator.userAgent)){
   console.log("Redirecting to mobile version...");
   location.replace('https://aqurik.fun/mobile');
 };
