0000330622018e5dd86d32142eb482c87ee1c608
aqurik <aqurik.men@gmail.com>
2025-05-10 08:45:00 +0300

    Add Redirects

---
+++ cool/index.js
@@ -0,0 +1,19 @@
+var NoHttps = false; // http mode (Used for testing and local hosting)
+
+// Redirects
+
+// redirects from http to https
+if (location.protocol !== 'https:'){
+  if (!NoHttps){
+    console.log("Redirecting to https...");
+    location.replace('https://' + location.href.split('//')[1]);
+  }
+}
+
+// redirects to mobile version
+if (/Mobi|Android|iPhone|Tablet|iPad|iPod/i.test(navigator.userAgent)) {
+  if (!NoHttps){
+    console.log("Redirecting to mobile version...");
+    window.location.href = 'https://aqurik.fun/mobile';
+  }
+}
--- projects/index.js
+++ projects/index.js
@@ -1,11 +1,47 @@
 // oh god forgive me
 
+
+var NoHttps = false; // http mode (Used for testing and local hosting)
+
+// Redirects
+
+// redirects from http to https
+if (location.protocol !== 'https:'){
+  if (!NoHttps){
+    console.log("Redirecting to https...");
+    location.replace('https://' + location.href.split('//')[1]);
+  }
+}
+
+// redirects to mobile version
+if (/Mobi|Android|iPhone|Tablet|iPad|iPod/i.test(navigator.userAgent)) {
+  if (!NoHttps){
+    console.log("Redirecting to mobile version...");
+    window.location.href = 'https://aqurik.fun/mobile';
+  }
+}
+
 function termlog(textf) {
-    const div = document.createElement("div");
-    const text = document.createTextNode(textf);
-    const outputs = document.getElementById("terminal-output");
+    div = document.createElement("div");
+    text = document.createTextNode(textf);
+    outputs = document.getElementById("terminal-output");
     div.className = "terminalLog";
     div.appendChild(text);
+    outputs.appendChild(div);
+}
+
+function termlink(textf, url) {
+    div = document.createElement("div");
+    link = document.createElement("a");
+    outputs = document.getElementById("terminal-output");
+
+    console.log("url: ", url);
+    link.className = "Link"; 
+    link.textContent = textf;
+    link.href = url;
+    link.target = "_blank"; 
+    div.className = "terminalLog";
+    div.appendChild(link);
     outputs.appendChild(div);
 }
 
@@ -23,6 +59,7 @@ function helpfr(){
     termlog(textf="cd <dir> - Changes directory (Ex: cd home | cd cool | cd /projects)")
     termlog(textf="clear - clears output (Ex: clear)")
     termlog(textf="echo - send text (Ex: echo wow | echo I love gd cologne)")
+    termlog(textf="hide-easy - hides easy mode (Ex: hide-easy)")
     termlog(textf="help - displays help message (Ex: help)")
     termlog(textf="")
 }
@@ -35,11 +72,21 @@ function ls(){
     termlog(textf="")
 }
 
+document.getElementById("easy").onclick = function(){
+    termlog("");
+    termlink("Home Page", "https://aqurik.fun");
+    termlink("Projects Page","https://aqurik.fun/projects")
+    termlink("The Cooler Homepage", "https://aqurik.fun/cool");
+    termlink("Mobile Redirect", "https://aqurik.fun/mobile");
+    termlog("");
+}
+
 
 document.getElementById("input").addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
         command = event.target.value.trim("");
-        document.getElementById("projectsThing").textContent = "Aqurik.fun"
+        document.getElementById("projectsThing").textContent = "Aqurik.fun";
+        document.getElementById("easy").style.marginLeft = "48em";
         event.target.value = ""; 
 
         if (command.startsWith("cd")){
@@ -81,8 +128,12 @@ document.getElementById("input").addEventListener("keydown", (event) => {
             ls();
         }
         else if (command.startsWith("echo")){
-            let echot = command.replace("echo", "")
-            termlog(textf=echot)
+            let echot = command.replace("echo", "");
+            termlog(textf=echot);
+        }
+
+        else if (command.startsWith("hide-easy")){
+            document.getElementById("easy").style.display = "none";
         }
         else{
             help();
